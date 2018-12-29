bc72354b2f263e1ebe18e00b25db48d5481440a2
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-29 18:25:48 +0100

    create project via cli tool
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+	</div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import VueJsonPretty from "vue-json-pretty";
+
+export default {
+	name: "app",
+	components: {
+		HelloWorld,
+		VueJsonPretty
+	}
+};
+</script>
+
+ <vue-json-pretty
+      :path="'res'"
+      :data="{ key: 'value' }">
+    </vue-json-pretty>
+
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
